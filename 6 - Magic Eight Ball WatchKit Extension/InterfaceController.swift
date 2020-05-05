70a4fa046153b36dbeeba3a1a3d2e8f246849eac
Brice Brinson <blbrinson@outlook.com>
2020-05-05 09:14:26 -0500

    Initial Commit

---
+++ 6 - Magic Eight Ball WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  6 - Magic Eight Ball WatchKit Extension
+//
+//  Created by Brice on 5/5/20.
+//  Copyright © 2020 blbrinson. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
